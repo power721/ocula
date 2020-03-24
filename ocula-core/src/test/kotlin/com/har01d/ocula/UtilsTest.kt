bedf3d71d6c5fc3a1f3e87274ae32522e5d47a96
Harold <power721@163.com>
2020-03-24 11:56:46 +0800

    update UtilsTest

--- ocula-core/src/test/kotlin/com/har01d/ocula/UtilsTest.kt
+++ ocula-core/src/test/kotlin/com/har01d/ocula/UtilsTest.kt
@@ -1,5 +1,7 @@
 package com.har01d.ocula
 
+import com.har01d.ocula.util.generateId
+import com.har01d.ocula.util.md5
 import com.har01d.ocula.util.normalizeUrl
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -31,5 +33,18 @@ class UtilsTest {
         assertEquals("https://example.com/test.html", normalizeUrl("https://example.com", "/test.html"))
         assertEquals("https://example.com/?q=image", normalizeUrl("https://example.com", "?q=image"))
         assertEquals("https://image.com/test.png", normalizeUrl("https://example.com", "//image.com/test.png"))
+    }
+
+    @Test
+    fun testMd5() {
+        assertEquals("098f6bcd4621d373cade4e832627b4f6", "test".md5())
+        assertEquals("202cb962ac59075b964b07152d234b70", "123".md5())
+        assertEquals("a7bac2239fcdcb3a067903d8077c4a07", "中文".md5())
+    }
+
+    @Test
+    fun testGenerateId() {
+        assertEquals(6, generateId(6).length)
+        assertEquals(10, generateId(10).length)
     }
 }
